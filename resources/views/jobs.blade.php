5a9cc45fbc8349edfad8df11052e446ba864a4b9
Jerry Vargas <>
2024-04-06 00:21:27 -0400

    adding eager loading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,12 +3,15 @@
         About Page
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach($jobs as $job)
-            <a href="/jobs/{{$job['id']}}" class="text-blue-400 hover:underline">
-                <li><strong>{{$job['title']}}</strong> Pays {{$job['salary']}} per year.</li>
+            <a href="/jobs/{{$job['id']}}" class="block px-4 border border-gray-200 rounded">
+                <div class="font-bold text-blue-500 text-sm">{{$job->employer->name}}</div>
+                <div>
+                    <strong>{{$job['title']}}</strong> Pays {{$job['salary']}} per year.
+                </div>
             </a>
         @endforeach
-    </ul>
+    </div>
 
 </x-layout>
