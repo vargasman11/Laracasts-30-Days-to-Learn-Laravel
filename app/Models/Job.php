0da4204fa8cdd2bcda4173d35a326639e565e526
Jerry Vargas <>
2024-04-12 00:24:29 -0400

    add

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job extends Model
-{
+class Job extends Model {
     use HasFactory;
 
     protected $table = 'job_listings';
