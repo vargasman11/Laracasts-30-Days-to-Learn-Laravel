09f9adb552a25b60ac861cf7528a8fe0c88d6c46
Jerry Vargas <vargasman11@gmail.com>
2024-08-28 13:56:27 -0500

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,16 @@ export default {
     extend: {
       colors: {
         "black": "#060606"
+      },
+      fontFamily:{
+        "hanken-grotesk":["Hanken Grotesk","sans-serif"]
+      },
+      fontSize:{
+        "2xs":".625rem"
       }
     },
   },
   plugins: [],
+  mode: 'jit',
 }
 
