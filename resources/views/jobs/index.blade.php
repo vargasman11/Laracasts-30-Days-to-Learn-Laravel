ca265f5e8b9c803c9e10a7af378962391a0fe371
Jerry Vargas <vargasman11@gmail.com>
2024-08-30 23:50:45 -0500

    add

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -3,10 +3,14 @@
         <section class="text-center pt-6">
             <h1 class="font-bold text-4xl">Let's Find your next job</h1>
 
-            <form action="" class="mt-6">
-                <input type="text" placeholder="Web Developer..."
-                       class="rounded-xl bg-white/5 border-white/10 px-5 py-4 max-w-xl">
-            </form>
+{{--            <form action="" class="mt-6">--}}
+{{--                <input type="text" placeholder="Web Developer..."--}}
+{{--                       class="rounded-xl bg-white/5 border-white/10 px-5 py-4 max-w-xl">--}}
+{{--            </form>--}}
+            <x-forms.form action="/search" class="mt-6">
+                <x-forms.input :label="false" name="q" placeholder="Web Developer..." />
+            </x-forms.form>
+
         </section>
 
         <section class="pt-10">
